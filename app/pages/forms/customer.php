b8cccd4b43d95b35fcd1cf94c259aa23c9df3de6
Nesar <rronyy@gmail.com>
2026-09-02 22:46:09 +0800

    add customer map

--- app/pages/forms/customer.php
+++ app/pages/forms/customer.php
@@ -21,6 +21,8 @@ if (isset($post->save)) {
     if (isset($post->location))
       $obj->location = $post->location;
     $obj->city = $post->city;
+    if (isset($post->map))
+      $obj->map = $post->map;
     if (isset($post->email))
       $obj->email = $post->email;
     $obj->branch_id = $branch_id;
@@ -114,6 +116,7 @@ $html .= "</table></td></tr>
               'contact' => ['col' => 6, 'label' => 'Contact Person', 'type' => 'text', 'value' => $obj->contact, 'required' => true],
               'mobile' => ['col' => 6, 'label' => 'C.P. Mobile', 'type' => 'text', 'value' => $obj->mobile, 'required' => true],
               'city' => ['col' => 6, 'label' => 'Area', 'type' => 'dropdown', 'value' => $obj->city, 'table' => 'city', 'valueField' => 'name', 'filter' => 'branch_id = ' . $branch_id, 'required' => true, 'sort'=>'name'],
+              'map' => ['col' => 12, 'label' => 'Map', 'type' => 'text', 'value' => $obj->map, 'required' => false],
               'html' => ['col' => 6, 'type' => 'html', 'html' => $html],
               'image' => ['col' => 6, 'label' => 'Photo', 'type' => 'image', 'value' => $obj->image, 'required' => false],
               // 'location' => ['col' => 4, 'label' => 'Location', 'type' => 'text', 'value' => $obj->location, 'required' => true],
